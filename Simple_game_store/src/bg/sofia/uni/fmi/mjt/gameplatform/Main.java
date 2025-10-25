d522f8d560a46e2d3fc669e83602932ceef43c7b
Kerem Halil <keremsunayhalil@gmail.com>
2025-10-25 21:27:48 +0300

    Remove unnecessary comments

--- Simple_game_store/src/bg/sofia/uni/fmi/mjt/gameplatform/Main.java
+++ Simple_game_store/src/bg/sofia/uni/fmi/mjt/gameplatform/Main.java
@@ -2,30 +2,25 @@ package bg.sofia.uni.fmi.mjt.gameplatform;
 import bg.sofia.uni.fmi.mjt.gameplatform.store.GameStore;
 import bg.sofia.uni.fmi.mjt.gameplatform.store.item.StoreItem;
 import bg.sofia.uni.fmi.mjt.gameplatform.store.item.filter.*;
-import bg.sofia.uni.fmi.mjt.gameplatform.store.item.category.Game; // or wherever your Game class is
+import bg.sofia.uni.fmi.mjt.gameplatform.store.item.category.Game;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class Main {
-    public static void main(String[] args) {
-        // Create some test items
+    public static void main(String[] args)
+    {
         StoreItem game1 = new Game("Elden Ring", BigDecimal.valueOf(59.99), LocalDateTime.of(2022, 2, 25, 0, 0), "horror");
         StoreItem game2 = new Game("Mario Kart", BigDecimal.valueOf(39.99), LocalDateTime.of(2017, 4, 28, 0, 0), "action");
         StoreItem game3 = new Game("Hollow Knight", BigDecimal.valueOf(14.99), LocalDateTime.of(2017, 2, 24, 0, 0), "interactive");
 
-        // Add them to an array
         StoreItem[] items = { game1, game2, game3 };
 
-        // Create filters
         ItemFilter priceFilter = new PriceItemFilter(BigDecimal.valueOf(10), BigDecimal.valueOf(40));
         ItemFilter titleFilter = new TitleItemFilter("Knight", false);
         ItemFilter[] filters = { priceFilter, titleFilter };
 
-        // Create store
         GameStore store = new GameStore(items);
-
-        // Test filtering
         StoreItem[] results = store.findItemByFilters(filters);
 
         System.out.println("Filtered results:");
@@ -35,7 +30,7 @@ public class Main {
             }
         }
 
-        // Test discount
+        // Discount tests
         store.applyDiscount("VAN40");
         System.out.println("\nAfter applying VAN40 discount:");
         for (StoreItem item : store.getAvailableItems()) {
